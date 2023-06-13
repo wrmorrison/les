84df8e6d455016c3b8035677115b4fb05a432a5d
wrmorrison <wrmorrison@gmail.com>
2023-06-12 18:33:37 -1000

    22

---
+++ includes.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // Fetch the bannernav HTML file
+  fetch('https://wrmorrison.github.io/les/includes/bannernav.html')
+  .then(response => response.text())
+  .then(html => {
+    // Insert the bannernav HTML into the container
+    document.getElementById('bannernavContainer').innerHTML = html;
+  });
+
+  // Fetch the hero HTML file
+  fetch('https://wrmorrison.github.io/les/includes/hero.html')
+  .then(response => response.text())
+  .then(html => {
+    // Insert the hero HTML into the container
+    document.getElementById('heroContainer').innerHTML = html;
+  });
+
+  // Fetch the footer HTML file
+  fetch('https://wrmorrison.github.io/les/includes/footer.html')
+  .then(response => response.text())
+  .then(html => {
+    // Insert the footer HTML into the container
+    document.getElementById('footerContainer').innerHTML = html;
+  });
+});
