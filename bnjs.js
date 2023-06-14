a891389c3d0723f68d438122890abf5539369975
wrmorrison <wrmorrison@gmail.com>
2023-06-14 10:53:26 -1000

    asf

---
+++ bnjs.js
@@ -0,0 +1,61 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function () {
+
+  // Fetch the bannernav HTML file
+  fetch('https://wrmorrison.github.io/les/includes/bannernav.html')
+    .then(response => response.text())
+    .then(html => {
+      // Insert the bannernav HTML into the container
+      document.getElementById('bannernavContainer').innerHTML = html;
+
+      // Apply the JavaScript logic to the elements within bannernav.html
+
+      // Dropdowns in navbar
+      var $dropdowns = getAll('.navbar-item.has-dropdown:not(.is-hoverable)');
+      if ($dropdowns.length > 0) {
+        $dropdowns.forEach(function ($el) {
+          $el.addEventListener('click', function (event) {
+            event.stopPropagation();
+            $el.classList.toggle('is-active');
+          });
+        });
+
+        document.addEventListener('click', function (event) {
+          closeDropdowns();
+        });
+      }
+
+      function closeDropdowns() {
+        $dropdowns.forEach(function ($el) {
+          $el.classList.remove('is-active');
+        });
+      }
+
+      // Close dropdowns if ESC pressed
+      document.addEventListener('keydown', function (event) {
+        var e = event || window.event;
+        if (e.keyCode === 27) {
+          closeDropdowns();
+        }
+      });
+
+      // Toggles
+      var $burgers = getAll('.burger');
+      if ($burgers.length > 0) {
+        $burgers.forEach(function ($el) {
+          $el.addEventListener('click', function () {
+            var target = $el.dataset.target;
+            var $target = document.getElementById(target);
+            $el.classList.toggle('is-active');
+            $target.classList.toggle('is-active');
+          });
+        });
+      }
+    });
+});
+
+// Functions
+function getAll(selector) {
+  return Array.prototype.slice.call(document.querySelectorAll(selector), 0);
+}
